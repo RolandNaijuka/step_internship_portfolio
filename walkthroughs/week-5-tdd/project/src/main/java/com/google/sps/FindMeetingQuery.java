17df083f8a7eba259299f18a6410b66096d0d4b8
RolandNaijuka <rnaijuka@u.rochester.edu>
2020-06-15 12:31:13 -0400

    Handle edge cases and call the helper function

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,34 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.HashSet;
+import java.util.List;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // Consider the edge cases where there is no attendes or the meeting duration is more than 24 hours or there are no known events
+
+    // Options where there are no Attendees
+    if (request.getAttendees().isEmpty())
+      return Arrays.asList(TimeRange.WHOLE_DAY);
+
+    // No options when meeting request is too long (more than a day)
+    if (request.getDuration() > TimeRange.WHOLE_DAY.duration())
+      return Arrays.asList();
+    
+    // No events
+    if (events.isEmpty()) {
+      // No events but there is a valid meeting request
+      if (request.getDuration() <= TimeRange.WHOLE_DAY.duration())
+        return Arrays.asList(TimeRange.WHOLE_DAY);
+      return Arrays.asList();
+    }
+
+    // Call the helper function for all other cases that no edge cases
+    return queryHelper(events, request);
   }
 }
